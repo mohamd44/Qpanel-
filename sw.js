de81d98a6cf4e7d3f62d3b4858a81ffd78f84314
mohamd44 <42225358+mohamd44@users.noreply.github.com>
2026-06-27 02:07:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'qpanell-v1.0.15';
+const CACHE = 'qpanell-v1.0.16';
 const ASSETS = [
   './', './index.html', './styles.css', './app.js',
   './logo.jpeg', './icon-192.png', './icon-512.png',
